219881847b04d039eb15a5844007ed22355940fe
vishnuvardhan <yvishnuvardhan325@gmail.com>
2026-03-01 16:16:58 +0530

    port changed

--- index.js
+++ index.js
@@ -308,5 +308,9 @@ if (rightLogo) {
   }
 );
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server running on", PORT));
+
+
 
